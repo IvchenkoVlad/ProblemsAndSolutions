86aa4773d6ab368064f51b55fa483fef560c5c9f
IvchenkoVlad <50472173+IvchenkoVlad@users.noreply.github.com>
2022-08-12 13:07:08 -0400

    Time: 51 ms (6.20%), Space: 53.9 MB (5.13%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
@@ -3,20 +3,16 @@ class Solution {
         //find out which type of boxes contains the most items
         Arrays.sort(boxTypes, (a,b)->b[1]-a[1]);
         System.out.println(Arrays.deepToString(boxTypes));
-        int totalItem = 0;
+        int totalItems = 0;
         for(int [] eachBox : boxTypes){
             if(truckSize == 0){
                 break;
             }
-            if(truckSize>eachBox[0]){
-                totalItem += (eachBox[0] * eachBox[1]);
-                truckSize-=eachBox[0];
-            }
-            else{
-                totalItem += (truckSize * eachBox[1]);
-                truckSize-=truckSize;
-            }
+            
+            int boxCount = Math.min(eachBox[0], truckSize);
+            totalItems += (boxCount*eachBox[1]);
+            truckSize-=boxCount;
         }
-        return totalItem;
+        return totalItems;
     }
 }
