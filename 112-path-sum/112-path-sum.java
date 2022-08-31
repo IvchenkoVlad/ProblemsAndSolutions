aa8044ee3ed73621443fb3259fda535f29fd848b
IvchenkoVlad <50472173+IvchenkoVlad@users.noreply.github.com>
2022-08-30 21:22:46 -0400

    Time: 1 ms (63.01%), Space: 44.1 MB (24.03%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -14,17 +14,18 @@
  * }
  */
 class Solution {
+    
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root == null){
             return false;
         }
         targetSum -= root.val;
-        
         if(root.left == null && root.right == null){
             if(targetSum == 0){
                 return true;
             }
         }
+        
         return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
     }
 }
